2396bf0aa33d59ad5128e6c7b2843ab585d3b337
Cosma Shalizi <cshalizi@cmu.edu>
2013-06-18 16:08:07 -0400

    Added licensing statement (2-clause simplified BSD)

--- smoothing-graphs.R
+++ smoothing-graphs.R
@@ -1,5 +1,6 @@
 # Code for learning graphon models by smoothing adjacency matrices
 # Cosma Shalizi, after conversation with Brian Karrer 8 May 2013
+  # See end of file for license (2-claused simplified BSD license)
 
 
 require(igraph)
@@ -157,3 +158,36 @@ uniformize <- function(x) {
 gaussianize <- function(x) {
   return(qnorm(uniformize(x)))
 }
+
+
+
+####################################
+#      LICENSE                     #
+####################################
+
+# Copyright (c) 2013, Cosma Shalizi
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met: 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer. 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution. 
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# The views and conclusions contained in the software and documentation are
+# those of the authors and should not be interpreted as representing official
+# policies, either expressed or implied, of their respective employers.
